dfcc9814c9370e0fee400938ce506f430c4ebd63
Daniel Ornelas <dornelas4@miners.utep.edu>
2020-05-19 00:47:20 -0500

    finally solved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CvComponent } from './cv/cv.component';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+
+const routes : Routes = [
+  {path : '', component : HomeComponent},
+  { path : 'home', component : HomeComponent},
+  { path : 'cv', component : CvComponent},
+  { path : 'about-me', component : AboutMeComponent},
+  { path : 'not-found', component : PageNotFoundComponent},
+  { path : '**' , redirectTo : '/not-found'}
+];
 
 @NgModule({
-
   imports: [RouterModule.forRoot(routes,{useHash : true})],
   exports: [RouterModule]
 })
