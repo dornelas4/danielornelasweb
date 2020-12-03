f2a3804e5b15c3d346aaf0be074fc4695e4e3635
Daniel Ornelas <dornelas4@miners.utep.edu>
2020-12-02 21:11:28 -0600

    privacy policy for white elephant

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CvComponent } from './cv/cv.component';
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WhiteElephantPrivacyComponent } from './white-elephant-privacy/white-elephant-privacy.component';
 
 
 const routes : Routes = [
@@ -11,6 +12,7 @@ const routes : Routes = [
   { path : 'home', component : HomeComponent},
   { path : 'cv', component : CvComponent},
   { path : 'about-me', component : AboutMeComponent},
+  { path : 'white-elephant-privacy',component : WhiteElephantPrivacyComponent},
   { path : 'not-found', component : PageNotFoundComponent},
   { path : '**' , redirectTo : '/not-found'}
 ];
---
+++ src/app/white-elephant-privacy/white-elephant-privacy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-white-elephant-privacy',
+  templateUrl: './white-elephant-privacy.component.html',
+  styleUrls: ['./white-elephant-privacy.component.scss']
+})
+export class WhiteElephantPrivacyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
